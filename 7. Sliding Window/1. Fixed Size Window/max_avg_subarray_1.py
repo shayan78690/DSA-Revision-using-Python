597a7b6bf5bd992f52e39ae2b4244be33612762e
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-11-15 18:53:17 +0530

    Update max_avg_subarray_1.py

--- 7. Sliding Window/1. Fixed Size Window/max_avg_subarray_1.py
+++ 7. Sliding Window/1. Fixed Size Window/max_avg_subarray_1.py
@@ -8,18 +8,17 @@ class Solution(object):
         n = len(nums)
         left = 0
         right = 0
-        maxi = float('-inf')
-        total = 0.0
+        maxavg = float('-inf')
+        s = 0.0
         while right < n:
-            total += nums[right]
-
-            while right-left+1 > k:
-                total -= nums[left]
+            s += nums[right]
+            if right-left+1 > k :
+                s -= nums[left]
                 left += 1
             
             if right-left+1 == k:
-                avg = total / k
-                maxi = max(maxi, avg)
-            
+                avg = s / k
+                maxavg = max(maxavg, avg)
             right += 1
-        return maxi
+        
+        return maxavg
