1120ec396f04de1b3b067e55a6364d021e6f8232
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-11-16 17:49:08 +0530

    Update count_nice.py

--- 7. Sliding Window/2. Variable Size Window (Longest subarray or substring with condition)/count_nice.py
+++ 7. Sliding Window/2. Variable Size Window (Longest subarray or substring with condition)/count_nice.py
@@ -1,24 +1,25 @@
 class Solution(object):
+    def numberOfSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        return self.func(nums, k) - self.func(nums, k-1)
     
     def func(self, nums, k):
-        count = 0
+        n = len(nums)
         left = 0
         right = 0
-        n = len(nums)
-        sum = 0
-        
+        count = 0
+        odd = 0
         while right < n:
-            sum += nums[right] % 2
-            
-            while sum > k:
-                sum -= nums[left] % 2
+            if nums[right] % 2 != 0:
+                odd += 1
+            while odd > k:
+                if nums[left] % 2 != 0:
+                    odd -= 1
                 left += 1
-            
-            count += (right - left + 1)
+            count += (right-left+1)
             right += 1
-        
         return count
-
-    def numberOfSubarrays(self, nums, k):
-        return self.func(nums, k) - self.func(nums, k - 1)
-        
