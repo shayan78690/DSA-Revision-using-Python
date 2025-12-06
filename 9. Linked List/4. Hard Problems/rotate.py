5c69889a1ecb4a8cbff0b71993ee83bb3c5506ab
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-06 15:14:33 +0530

    Update rotate.py

--- 9. Linked List/4. Hard Problems/rotate.py
+++ 9. Linked List/4. Hard Problems/rotate.py
@@ -1,33 +1,35 @@
-def rotateRight(head, k):
-    if head == None or head.next == None or k == 0:
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: Optional[ListNode]
+        :type k: int
+        :rtype: Optional[ListNode]
+        """
+        if not head or k == 0:
+            return head
+        
+        size = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            size += 1
+        
+        k = k % size
+        if k == 0:
+            return head
+        
+        tail.next = head
+         
+        steps_to_newhead = size-k-1
+        newtail = head
+        while steps_to_newhead > 0:
+            newtail = newtail.next
+            steps_to_newhead -= 1
+        head = newtail.next
+        newtail.next = None
         return head
-    # calculating length
-    temp = head
-    length = 1
-    while temp.next != None:
-        length += 1
-        temp = temp.next
-    # link last node to first node
-    temp.next = head
-    k = k % length  # when k is more than length of list
-    end = length - k  # to get end of the list
-    while end:
-        temp = temp.next
-        end -= 1
-    # breaking last node link and pointing to NULL
-    head = temp.next
-    temp.next = None
-
-
-    return head
-
-
-
-
-# utility function to print list
-def printList(head):
-    while head.next != None:
-        print(head.val, end='->')
-        head = head.next
-    print(head.val)
-    return
