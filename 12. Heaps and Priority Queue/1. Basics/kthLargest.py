b2e6529e03d80c6e85f30ff1c7f202441c522a7a
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-11 11:19:57 +0530

    Update kthLargest.py

--- 12. Heaps and Priority Queue/1. Basics/kthLargest.py
+++ 12. Heaps and Priority Queue/1. Basics/kthLargest.py
@@ -1,1 +1,13 @@
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums, k):
+        pq = []  
+
+        for num in nums:
+            heapq.heappush(pq, num)
+            if len(pq) > k:
+                heapq.heappop(pq)
+
+        return pq[0]
 
