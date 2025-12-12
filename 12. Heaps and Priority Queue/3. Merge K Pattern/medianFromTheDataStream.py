b7e00966dd92333d0e04cf42c13b4aa19d93b8bb
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-12 11:25:12 +0530

    Update medianFromTheDataStream.py

--- 12. Heaps and Priority Queue/3. Merge K Pattern/medianFromTheDataStream.py
+++ 12. Heaps and Priority Queue/3. Merge K Pattern/medianFromTheDataStream.py
@@ -1,3 +1,33 @@
+class MedianFinder(object):
+
+    def __init__(self):
+        self.nums = []
+
+    def addNum(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        self.nums.append(num)
+        
+
+    def findMedian(self):
+        """
+        :rtype: float
+        """
+        self.nums.sort()
+        n = len(self.nums)
+        if n % 2 != 1:
+            return self.nums[n // 2]
+        return (self.nums[n // 2 - 1] + self.nums[n // 2]) // 2.0
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+
 
 import heapq
 
