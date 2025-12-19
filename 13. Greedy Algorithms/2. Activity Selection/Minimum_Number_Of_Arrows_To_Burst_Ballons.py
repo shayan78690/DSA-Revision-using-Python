ebee5685feee9bd71776e62ab9d502f8d0c9737b
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-19 17:06:39 +0530

    Create Minimum_Number_Of_Arrows_To_Burst_Ballons.py

---
+++ 13. Greedy Algorithms/2. Activity Selection/Minimum_Number_Of_Arrows_To_Burst_Ballons.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+
+        points.sort(key=lambda x: x[1])
+
+        r = points[0][1]  
+        count = 1  
+
+        for i in range(1, len(points)):
+            if points[i][0] > r:
+                count += 1
+                r = points[i][1]  
+
+        return count
