38ef52a590046e0849d863ec211d953a4ff0b108
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-19 16:55:50 +0530

    Update N_Meetings_In_A_Room.py

--- 13. Greedy Algorithms/2. Activity Selection/N_Meetings_In_A_Room.py
+++ 13. Greedy Algorithms/2. Activity Selection/N_Meetings_In_A_Room.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def maximumMeetings(self,start,end):
+        n = len(start)
+        meetings = []
+        for i in range(n):
+            meetings.append([start[i], end[i]])
+        
+        meetings.sort(key = lambda x : x[1])
+        
+        lastEnd = -1
+        count = 0
+        for s, e in meetings:
+            if s > lastEnd:
+                count += 1
+                lastEnd = e
+                
+        return count
