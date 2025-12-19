eae6735075b11c0fada376732c8ad8971f2283c6
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-19 15:14:54 +0530

    Update Valid_Parenthesis_String.py

--- 13. Greedy Algorithms/1. Basics/Valid_Parenthesis_String.py
+++ 13. Greedy Algorithms/1. Basics/Valid_Parenthesis_String.py
@@ -1,51 +1,27 @@
-class Solution:
-
-    # Function to check if the string is valid using greedy logic
-    def checkValidString(self, s: str) -> bool:
-
-        # Variable to store minimum number of open brackets possible
-        min_open = 0
-
-        # Variable to store maximum number of open brackets possible
-        max_open = 0
-
-        # Traverse each character in the string
-        for char in s:
-
-            # If current character is '('
-            if char == '(':
-                min_open += 1
-                max_open += 1
-
-            # If current character is ')'
-            elif char == ')':
-                min_open -= 1
-                max_open -= 1
-
-            # If current character is '*', it can be '(', ')' or ''
+class Solution(object):
+    def checkValidString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        minopen = 0
+        maxopen = 0
+        for ch in s:
+            if ch == '(':
+                minopen += 1
+                maxopen += 1
+            elif ch == ')':
+                minopen -= 1
+                maxopen -= 1
             else:
-                 # If '*' acts as ')'
-                min_open -= 1    
-                # If '*' acts as '('
-                max_open += 1      
-
-            # If max_open goes below 0, we have too many unmatched ')'
-            if max_open < 0:
+                minopen -= 1
+                maxopen += 1
+            
+            if maxopen < 0:
                 return False
-
-            # min_open should not be negative
-            if min_open < 0:
-                min_open = 0
-
-        # At the end, all opens must be matched for valid string
-        return min_open == 0
-
-
-# Driver code
-if __name__ == "__main__":
-    s = input("Enter the string: ")
-    sol = Solution()
-    if sol.checkValidString(s):
-        print("Valid parenthesis string")
-    else:
-        print("Invalid parenthesis string")
+            
+            if minopen < 0:
+                minopen = 0
+        
+        return minopen == 0
+        
