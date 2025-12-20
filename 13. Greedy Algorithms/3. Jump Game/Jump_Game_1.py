c0b87caafcef197085aba852f0ec7d1be1b7edd1
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-20 11:29:44 +0530

    Update Jump_Game_1.py

--- 13. Greedy Algorithms/3. Jump Game/Jump_Game_1.py
+++ 13. Greedy Algorithms/3. Jump Game/Jump_Game_1.py
@@ -1,1 +1,13 @@
-
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        maxIdx = 0
+        for i in range(len(nums)):
+            if i > maxIdx:
+                return False
+            maxIdx = max(maxIdx, nums[i] + i)
+        return True
+        
