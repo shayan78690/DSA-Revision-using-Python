93ab1809c5a89dd90211b50eb7f5bd8958bc313e
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-20 21:56:14 +0530

    Update Remove_Covered_Intervals.py

--- 13. Greedy Algorithms/6. Intervals/Remove_Covered_Intervals.py
+++ 13. Greedy Algorithms/6. Intervals/Remove_Covered_Intervals.py
@@ -1,1 +1,16 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals):
+        # Step 1: sort intervals
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+
+        count = 0
+        max_end = 0
+
+        # Step 2: traverse intervals
+        for start, end in intervals:
+            if end > max_end:
+                count += 1
+                max_end = end
+
+        return count
 
