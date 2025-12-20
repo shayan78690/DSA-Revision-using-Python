e1fb792a4f32f6cd529bd2158425cf77d00f6180
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-20 21:51:07 +0530

    Update Non_Overlapping_Intervals.py

--- 13. Greedy Algorithms/6. Intervals/Non_Overlapping_Intervals.py
+++ 13. Greedy Algorithms/6. Intervals/Non_Overlapping_Intervals.py
@@ -1,1 +1,16 @@
-
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        intervals.sort(key = lambda x : x[1])
+        count = 0
+        lastEnd = intervals[0][1]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < lastEnd:
+                count += 1
+            else:
+                lastEnd = intervals[i][1]
+        return count
+        
