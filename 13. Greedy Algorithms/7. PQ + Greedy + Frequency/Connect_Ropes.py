773afddf27cef9e7a579fb6b74f1264fc38ae073
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-20 19:52:54 +0530

    Update Connect_Ropes.py

--- 13. Greedy Algorithms/7. PQ + Greedy + Frequency/Connect_Ropes.py
+++ 13. Greedy Algorithms/7. PQ + Greedy + Frequency/Connect_Ropes.py
@@ -1,1 +1,17 @@
+import heapq
 
+class Solution:
+    def minCost(self, arr):
+        pq = []
+        for a in arr:
+            heapq.heappush(pq, a)
+        
+        cost = 0
+        while len(pq) > 1:
+            first = heapq.heappop(pq)
+            second = heapq.heappop(pq)
+            s = first + second
+            cost += s
+            heapq.heappush(pq, s)
+
+        return cost
