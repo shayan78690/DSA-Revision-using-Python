74c4968f549260994e74195013c1ca1bfb7f208e
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-20 12:11:52 +0530

    Update Coin_Change.py

--- 13. Greedy Algorithms/4. Change Making and Coins/Coin_Change.py
+++ 13. Greedy Algorithms/4. Change Making and Coins/Coin_Change.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def findMin(self, n):
+        coins = [10, 5, 2, 1]
+        count = 0
+        
+        for coin in coins:
+            if n >= coin:
+                count += n // coin
+                n = n % coin
+        
+        return count
+        
