e0805f94b3b73a394b24c989730b770e96d610cf
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-20 19:09:06 +0530

    Update Last_Stone.py

--- 13. Greedy Algorithms/5. Scheduling and Deadline/Last_Stone.py
+++ 13. Greedy Algorithms/5. Scheduling and Deadline/Last_Stone.py
@@ -1,1 +1,15 @@
+import heapq
 
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        maxheap = []
+        for stone in stones:
+            heapq.heappush(maxheap, -stone)
+        
+        while len(maxheap) > 1:
+            first = -heapq.heappop(maxheap)
+            second = -heapq.heappop(maxheap)
+            if first != second:
+                new_weight = first-second
+                heapq.heappush(maxheap, -new_weight)
+        return -maxheap[0] if maxheap else 0
