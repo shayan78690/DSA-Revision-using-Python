2c5c56d29dd6dce2bca9b09542f15fa245fa3ba4
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-20 19:35:20 +0530

    Update Fractional_Knapsack.py

--- 13. Greedy Algorithms/5. Scheduling and Deadline/Fractional_Knapsack.py
+++ 13. Greedy Algorithms/5. Scheduling and Deadline/Fractional_Knapsack.py
@@ -1,1 +1,31 @@
+class Item:
+    def __init__(self, value, weight):
+        self.value = value
+        self.weight = weight
+        self.ratio = value / weight
+
+
+class Solution:
+    # Function to get the maximum total value in the knapsack
+    def fractionalKnapsack(self, val, wt, capacity):
+        n = len(val)
+        items = []
+
+        for i in range(n):
+            items.append(Item(val[i], wt[i]))
+
+        # sort items by value/weight ratio (descending)
+        items.sort(key=lambda x: x.ratio, reverse=True)
+
+        totalVal = 0.0
+
+        for item in items:
+            if capacity >= item.weight:
+                totalVal += item.value
+                capacity -= item.weight
+            else:
+                totalVal += item.ratio * capacity
+                break
+
+        return totalVal
 
