eb0145ba493dcd5cd4420df719d9d13b3de09f9b
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-12-20 19:26:30 +0530

    Update Max_Performance_Of_A_Team.py

--- 13. Greedy Algorithms/5. Scheduling and Deadline/Max_Performance_Of_A_Team.py
+++ 13. Greedy Algorithms/5. Scheduling and Deadline/Max_Performance_Of_A_Team.py
@@ -1,1 +1,20 @@
+import heapq
 
+class Solution(object):
+    def maxPerformance(self, n, speed, efficiency, k):
+        mod = 10**9 + 7
+        engineers = list(zip(efficiency, speed))
+        engineers.sort(reverse = True)
+        
+        maxheap = []
+        speedSum = 0
+        ans = 0
+        for eff, speed in engineers:
+            speedSum += speed
+            heapq.heappush(maxheap, speed)
+
+            if len(maxheap) > k:
+                speedSum -= heapq.heappop(maxheap)
+
+            ans = max(ans, speedSum * eff)
+        return ans % mod
