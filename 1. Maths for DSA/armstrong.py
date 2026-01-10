1261c159bbce2d46c368e6750c8418b302120b5c
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-01-10 22:40:20 +0530

    Fix armstrongNumber to handle any digit count
    Refactor armstrongNumber to use dynamic digit count for Armstrong calculation.

--- 1. Maths for DSA/armstrong.py
+++ 1. Maths for DSA/armstrong.py
@@ -1,9 +1,21 @@
 class Solution:
-    def armstrongNumber (self, n):
+    def armstrongNumber(self, n):
+        digits = self.countDigits(n)
         num = n
         armstrong = 0
-        while num != 0:
+
+        while num > 0:
             ld = num % 10
-            armstrong += ld**3
-            num = num // 10
+            armstrong += ld ** digits
+            num //= 10
+
         return armstrong == n
+
+    def countDigits(self, n):
+        if n == 0:
+            return 1
+        count = 0
+        while n > 0:
+            count += 1
+            n //= 10
+        return count
