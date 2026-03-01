f864e98e1064ad0b0f3bdfd4c8475b918d352286
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-03-01 23:15:02 +0530

    Update prime_in_range.py

--- 1. Maths for DSA/prime_in_range.py
+++ 1. Maths for DSA/prime_in_range.py
@@ -1,4 +1,25 @@
 import math
+L = 2
+R = 20
+prime = [0] * (R+1)
+for i in range(2, R+1):
+    prime[i] = 1
+
+for i in range(2, int(math.sqrt(R)) + 1):
+    if prime[i] == 1:
+        for j in range(i*i, R+1, i):
+            prime[j] = 0
+
+count = 0
+for i in range(L, R+1):
+    if prime[i] == 1:
+        count += 1
+print(count)ac
+
+
+
+
+import math
 class Solution:
     def countPrimes(self,L,R):
         isPrime = [0] * (R+1)
