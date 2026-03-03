5591f2d893b365c72a2608e64ccaa441d89530e6
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-03-03 18:11:12 +0530

    Update bank_allocation.py

--- 3. Binary Search/2. Binary Search on Answers/bank_allocation.py
+++ 3. Binary Search/2. Binary Search on Answers/bank_allocation.py
@@ -1,29 +1,34 @@
-class Solution:
-    
-    def func(self, arr, k, pages):
-        count_Students = 1
-        pagesCount = 0
-        for i in range(len(arr)):
-            if pagesCount + arr[i] <= pages:
-                pagesCount += arr[i]
-            else:
-                count_Students += 1
-                pagesCount = arr[i]
-        return count_Students
-    
-    def findPages(self, arr, k):
-        n = len(arr)
-        if k > n:
-            return -1
-        low = max(arr)
-        high = sum(arr)
-        ans = -1
-        while low <= high:
-            mid = (low+high)//2
-            temp = self.func(arr, k, mid)
-            if temp <= k:
-                ans = mid
-                high = mid-1
-            else:
-                low = mid+1
-        return ans
+import math
+
+def tempFunc(arr, m, pages):
+    student = 1
+    total = 0
+    for i in range(len(arr)):
+        if arr[i] + total <= pages:
+            total += arr[i]
+        else:
+            student += 1
+            total = arr[i]
+    return student
+
+def func(arr, m):
+    n = len(arr)
+    if n < m:
+        return -1
+    low = max(arr)
+    high = sum(arr)
+    result = high
+    while low <= high:
+        mid = (low + high) // 2
+        ans = tempFunc(arr, m, mid)
+        if ans <= m:
+            result = mid
+            high = mid-1
+        else:
+            low = mid+1
+    return result
+
+arr = list(map(int, input().split(",")))
+m = int(input())
+result = func(arr, m)
+print(result)
