91796a8bc600069d90e2aff4afb27fbcc4395fec
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-03-03 17:38:56 +0530

    Update smallest_divisor.py

--- 3. Binary Search/2. Binary Search on Answers/smallest_divisor.py
+++ 3. Binary Search/2. Binary Search on Answers/smallest_divisor.py
@@ -1,27 +1,29 @@
 import math
-class Solution(object):
-    def func(self, arr, threshold, divisor):
-        s = 0
-        for i in range(len(arr)):
-            s += math.ceil(float(arr[i])/divisor)
-        return s
-    def smallestDivisor(self, nums, threshold):
-        """
-        :type nums: List[int]
-        :type threshold: int
-        :rtype: int
-        """
-        low = 1
-        high = max(nums)
-        ans = high
-        while low <= high:
-            mid = (low+high)//2
-            result = self.func(nums, threshold, mid)
-            if result <= threshold:
-                ans = mid
-                high = mid-1
-            else:
-                low = mid+1
 
-        return ans
-        
+def func2(arr, limit, divisor):
+    total = 0
+    for i in range(len(arr)):
+        total += math.ceil(arr[i] / divisor)
+    return total
+
+def func1(arr, limit):
+    low = 1
+    high = max(arr)
+    result = high
+    while low <= high:
+        mid = (low + high) // 2
+        ans = func2(arr, limit, mid)
+        if ans <= limit:
+            result = mid
+            high = mid-1
+        else:
+            low = mid+1
+    return result
+    
+    
+
+
+arr = list(map(int, input().split(",")))
+limit = int(input())
+result = func1(arr, limit)
+print(result)
