c33b6297e4b65f39a22a8485bf1bd66ea124aa0a
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-03-03 17:30:24 +0530

    Update minimum_days_to_make_m_boquet.py

--- 3. Binary Search/2. Binary Search on Answers/minimum_days_to_make_m_boquet.py
+++ 3. Binary Search/2. Binary Search on Answers/minimum_days_to_make_m_boquet.py
@@ -1,34 +1,36 @@
-class Solution(object):
+import math
+
+def func2(arr, m, k, day):
+    flowers = 0
+    boquet = 0
+    for i in range(len(arr)):
+        if arr[i] <= day:
+            flowers += 1
+            if flowers == k:
+                boquet += 1
+                flowers = 0
+        else:
+            flowers = 0
+    return boquet >= m
+
+def func1(arr, m, k):
+    if len(arr) < m*k:
+        return -1
+    low = min(arr)
+    high = max(arr)
+    result = high
+    while low <= high:
+        mid = (low + high) // 2
+        if func2(arr, m, k, mid):
+            result = mid
+            high = mid-1
+        else:
+            low = mid+1
+    return result
     
-    def possible(self, arr, m, k, mid):
-        count_of_days = 0
-        no_of_Boquets = 0
-        for i in range(len(arr)):
-            if arr[i] <= mid:
-                count_of_days += 1
-            else:
-                no_of_Boquets += (count_of_days//k)
-                count_of_days = 0
-        no_of_Boquets += (count_of_days//k)
-        return no_of_Boquets >= m
 
-    def minDays(self, bloomDay, m, k):
-        """
-        :type bloomDay: List[int]
-        :type m: int
-        :type k: int
-        :rtype: int
-        """
-        low = min(bloomDay)
-        high = max(bloomDay)
-        ans = -1
-        while low <= high:
-            mid = (low + high) // 2
-            if self.possible(bloomDay, m, k, mid):
-                ans = mid
-                high = mid-1
-            else:
-                low = mid+1
-        return ans
 
-        
+arr = list(map(int, input().split(",")))
+m, k = map(int, input().split(","))
+result = func1(arr, m, k)
+print(result)
