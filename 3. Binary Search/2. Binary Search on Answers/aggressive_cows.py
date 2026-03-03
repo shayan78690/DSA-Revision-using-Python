6bf31958ac1cf708d151e85c7c6082fc0d6ec87a
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-03-03 19:04:13 +0530

    Update aggressive_cows.py

--- 3. Binary Search/2. Binary Search on Answers/aggressive_cows.py
+++ 3. Binary Search/2. Binary Search on Answers/aggressive_cows.py
@@ -1,27 +1,31 @@
-class Solution:
-    
-    def possible(self, stalls, k, dist):
-        cows = 1
-        last = stalls[0]
-        for i in range(1, len(stalls)):
-            if stalls[i]-last >= dist:
-                cows += 1
-                last = stalls[i]
-            if cows >= k:
-                return True
-        return False
-    
-    def aggressiveCows(self, stalls, k):
-        stalls.sort()
-        low = 1
-        high = stalls[len(stalls)-1] - stalls[0]
-        ans = low
-        while low <= high:
-            mid = (low+high)//2
-            if self.possible(stalls, k, mid):
-                ans = mid
-                low = mid+1
-            else:
-                high = mid-1
-        return ans
-        
+def tempFunc(stalls, k, dist):
+    last = stalls[0]
+    cows = 1
+    for i in range(1, len(stalls)):
+        if stalls[i] - last >= dist:
+            cows += 1
+            last = stalls[i]
+            
+        if cows >= k:
+            return True
+    return False
+
+def func(stalls, k):
+    stalls.sort()
+    n = len(stalls)
+    low = 1
+    high = stalls[n-1] - stalls[0]
+    result = low 
+    while low <= high:
+        mid = (low + high) // 2
+        if tempFunc(stalls, k, mid):
+            result = mid
+            low = mid+1
+        else:
+            high = mid-1
+    return result
+
+stalls = list(map(int, input().split(",")))
+k = int(input())
+result = func(stalls, k)
+print(result)
