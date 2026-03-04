7745cfd7a45601aead368d170f7f4466734cc5d9
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-03-04 15:01:08 +0530

    Update count_rows_with_max_ones.py

--- 3. Binary Search/3. Binary Search on 2-D Array/count_rows_with_max_ones.py
+++ 3. Binary Search/3. Binary Search on 2-D Array/count_rows_with_max_ones.py
@@ -1,26 +1,27 @@
-class Solution:
-    
-    def func(self, arr, m, x):
-        low = 0
-        ans = m
-        high = len(arr)-1
-        while low <= high:
-            mid = (low + high) // 2
-            if arr[mid] >= x:
-                ans = mid
-                high = mid-1
-            else:
-                low = mid+1
-        return ans
-            
-    def rowWithMax1s(self, arr):
-        n = len(arr)
-        m = len(arr[0])
-        max_count = 0
-        index = -1
-        for i in range(n):
-            ones = m - self.func(arr[i], m, 1)
-            if ones > max_count:
-                max_count = ones
-                index = i
-        return index
+def lowerBound(arr, n, x):
+    low = 0
+    high = n-1
+    result = high
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] >= x:
+            result = mid
+            high = mid-1
+        else:
+            low = mid+1
+    return result
+
+def func(arr, n, m):
+    max_count = 0
+    index = -1
+    for i in range(n):
+        ones = m - lowerBound(arr[i], m, 1)
+        if ones > max_count:
+            max_count = ones
+            index = i
+    return index
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split(","))) for _ in range(n)]
+result = func(arr, n, m)
+print(result)
