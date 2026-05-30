ed23f763787a49284164e0dd9a97942089edde26
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-05-30 20:19:00 +0530

    Update power_exponentiation.py

--- 1. Maths for DSA/power_exponentiation.py
+++ 1. Maths for DSA/power_exponentiation.py
@@ -1,11 +1,11 @@
 class Solution:
-    def power(self, x: int, n: int) -> int:
+    def func(self, x, n):
         if n == 0:
             return 1
+        half = self.func(x, n//2)
+        return half*half if n % 2 == 0 else x*half*half
 
-        half = self.power(x, n // 2)
-
-        if n % 2 == 0:
-            return half * half
-        else:
-            return x * half * half
+x, n = map(int, input().split(" "))
+obj = Solution()
+result = obj.func(x, n)
+print(result)
