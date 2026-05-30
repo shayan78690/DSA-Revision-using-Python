a75e1797f6fa2b620fd6d0df0ed8065a3ecf3d16
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-05-31 01:56:58 +0530

    Update and rename 3. if subarray exist to 3. if_subarray_exist.py

--- 6. Prefix Sum/6. Zero sum logic/3. if_subarray_exist.py
+++ 6. Prefix Sum/6. Zero sum logic/3. if_subarray_exist.py
@@ -1,8 +1,6 @@
-#User function Template for python3
 
 class Solution:
     
-    #Function to check whether there is a subarray present with 0-sum or not.
     def subArrayExists(self,arr):
         n = len(arr)
         s = 0
