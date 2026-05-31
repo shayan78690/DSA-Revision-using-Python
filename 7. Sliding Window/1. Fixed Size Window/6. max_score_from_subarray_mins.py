9f5ec885b4b2f4119022654b9abd01448e736ee2
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-05-31 19:30:42 +0530

    Update 6. max_score_from_subarray_mins.py

--- 7. Sliding Window/1. Fixed Size Window/6. max_score_from_subarray_mins.py
+++ 7. Sliding Window/1. Fixed Size Window/6. max_score_from_subarray_mins.py
@@ -9,3 +9,20 @@ class Solution:
         return maxi
             
     
+class Solution:
+    def maxSum(self, arr):
+        n = len(arr)
+        left = 0
+        right = 0
+        maxi = 0
+        s = 0
+        while right < n:
+            s += arr[right]
+            if right-left+1 > 2:
+                s -= arr[left]
+                left += 1
+            if right-left+1 == 2:
+                maxi = max(maxi, s)
+            right += 1
+        return maxi
+    
