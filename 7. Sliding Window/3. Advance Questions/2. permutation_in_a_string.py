c772305bf4401ac4d3ca5c77a60af889dc7a1a06
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-05-31 21:41:53 +0530

    Update 2. permutation_in_a_string.py

--- 7. Sliding Window/3. Advance Questions/2. permutation_in_a_string.py
+++ 7. Sliding Window/3. Advance Questions/2. permutation_in_a_string.py
@@ -1,31 +1,22 @@
 class Solution(object):
     def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
-        n, m = len(s1), len(s2)
+        n = len(s1)
+        m = len(s2)
         if n > m:
             return False
-        count = [0] * 26
+        s1_freq = [0] * 26
         for i in range(n):
-            count[ord(s1[i]) - ord('a')] += 1
-            count[ord(s2[i]) - ord('a')] -= 1
+            s1_freq[ord(s1[i])-ord('a')] += 1
         
-        if self.allZeroes(count):
-            return True
-        
-        for i in range(n, m):
-            count[ord(s2[i]) - ord('a')] -= 1
-            count[ord(s2[i - n]) - ord('a')] += 1
-
-            if self.allZeroes(count):
-                return True
+        window_freq = [0] * 26
+        left = 0
+        for right in range(m):
+            window_freq[ord(s2[right])-ord('a')] += 1
+            while right-left+1 > n:
+                window_freq[ord(s2[left])-ord('a')] -= 1
+                left += 1
+            if right-left+1 == n:
+                if window_freq == s1_freq:
+                    return True
         return False
-
-    def allZeroes(self, count):
-        for i in range(len(count)):
-            if count[i] != 0:
-                return False
-        return True
+        
