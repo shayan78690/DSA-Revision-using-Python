25639fb8cf1b4aceb05e1deb170b7d78aa32b282
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-05-31 21:33:25 +0530

    Update 5. count anagrams.py

--- 7. Sliding Window/3. Advance Questions/5. count anagrams.py
+++ 7. Sliding Window/3. Advance Questions/5. count anagrams.py
@@ -24,7 +24,31 @@ class Solution:
 	            if self.anagram(txt[left:right+1], pat):
 	                count += 1
 
-
+class Solution:
+	    
+	def search(self,pat, txt):
+	    n = len(txt)
+	    m = len(pat)
+	    if m > n:
+	        return 0
+	    pat_freq = [0] * 26
+	    for i in range(m):
+	        pat_freq[ord(pat[i])-ord('a')] += 1
+	    
+	    window_freq = [0] * 26
+	    left = 0
+	    count = 0
+	    for right in range(n):
+	        window_freq[ord(txt[right])-ord('a')] += 1
+	        while right-left+1 > m:
+	            window_freq[ord(txt[left])-ord('a')] -= 1
+	            left += 1
+	        if right-left+1 == m:
+	            if window_freq == pat_freq:
+	                count += 1
+	    
+	    return count
+	            
 
 
 	        right += 1
