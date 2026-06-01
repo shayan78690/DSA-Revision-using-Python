a09dd2e9a9054926555ac3fc3cac0143b7285e2c
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-01 22:35:03 +0530

    Update 2. remove_element.py

--- 8. Two pointers/2. In place edits and Duplicates/2. remove_element.py
+++ 8. Two pointers/2. In place edits and Duplicates/2. remove_element.py
@@ -7,3 +7,13 @@ class Solution(object):
         """
         while val in nums:
             nums.remove(val)
+class Solution(object):
+    def removeElement(self, nums, val):
+        n = len(nums)
+        i = 0
+        for j in range(n):
+            if nums[j] != element:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        return i
+        
