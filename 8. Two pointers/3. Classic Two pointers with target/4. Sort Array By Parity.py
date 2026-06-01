a7b593c277e1c3a9ded95278a619c74a4c58d8a3
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-01 23:14:18 +0530

    Update 4. Sort Array By Parity.py

--- 8. Two pointers/3. Classic Two pointers with target/4. Sort Array By Parity.py
+++ 8. Two pointers/3. Classic Two pointers with target/4. Sort Array By Parity.py
@@ -14,3 +14,14 @@ class Solution(object):
                 j += 1
         return nums
         
+
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        n = len(nums)
+        i = 0
+        for j in range(n):
+            if nums[j] % 2 == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        return nums
+        
