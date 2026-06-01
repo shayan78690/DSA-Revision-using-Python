089d1addfeb828503afd6783098c30d884aa1458
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-01 23:12:39 +0530

    Update 2. move_zeroes.py

--- 8. Two pointers/3. Classic Two pointers with target/2. move_zeroes.py
+++ 8. Two pointers/3. Classic Two pointers with target/2. move_zeroes.py
@@ -14,3 +14,14 @@ class Solution(object):
                 nums[i], nums[j] = nums[j], nums[i]
                 j += 1
         
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        n = len(nums)
+        i = 0
+        for j in range(n):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        return nums
+        
