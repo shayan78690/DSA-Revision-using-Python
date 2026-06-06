8fe909e0de8a51654916e3e873c81b43438ca05b
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-06 23:23:52 +0530

    Update 9. Valid Parentheses.py

--- 10. Stack and Queue/1. Basics and Implementation/9. Valid Parentheses.py
+++ 10. Stack and Queue/1. Basics and Implementation/9. Valid Parentheses.py
@@ -1,23 +1,16 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        n = len(s)
         stack = []
         for ch in s:
-            if ch in ['(', '[', '{']:
+            if ch in ['(', '{', '[']:
                 stack.append(ch)
             else:
-                if stack:
-                    if ((ch == ')' and stack[-1] == '(') or  (ch == ']' and stack[-1] == '[')
-                    or  (ch == '}' and stack[-1] == '{')):
+                if not stack:
+                    return False
+                else:
+                    if (ch == ')' and stack[-1] == '(') or (ch == '}' and stack[-1] == '{') or (ch == ']' and stack[-1] == '['):
                         stack.pop()
-                        continue
                     else:
                         return False
-                else:
-                    return False
-            
         return len(stack) == 0
+        
