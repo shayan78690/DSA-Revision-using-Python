f4929811a84dbd304aaf04331ec52466d18a613c
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-06 23:37:05 +0530

    Update 10. Duplicate Parenthesis.py

--- 10. Stack and Queue/1. Basics and Implementation/10. Duplicate Parenthesis.py
+++ 10. Stack and Queue/1. Basics and Implementation/10. Duplicate Parenthesis.py
@@ -22,3 +22,23 @@ class Solution:
                 stack.append(ch)
 
         return False
+
+
+from sys import *
+from collections import *
+from math import *
+
+def duplicateParanthesis(expr: str) -> bool:
+    stack = []
+    for ch in expr:
+        if ch == ')':
+            count = 0
+            while stack and stack[-1] != '(':
+                stack.pop()
+                count += 1
+            stack.pop()
+            if count < 1:
+                return True
+        else:
+            stack.append(ch)
+    return False
