d0350479316c79c22f5195f44acb371968df0ec3
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-06 22:35:25 +0530

    Update 8. min_stack.py

--- 10. Stack and Queue/1. Basics and Implementation/8. min_stack.py
+++ 10. Stack and Queue/1. Basics and Implementation/8. min_stack.py
@@ -29,9 +29,40 @@ class MinStack(object):
         
 
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(value)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+    def push(self, value):
+        self.stack.append(value)
+        if not self.minstack or self.minstack[-1] >= value:
+            self.minstack.append(value)
+    def pop(self):
+        if not self.stack:
+            return
+        if self.stack[-1] == self.minstack[-1]:
+            self.minstack.pop()
+        return self.stack.pop()
+    def top(self):
+        if not self.stack:
+            return 0
+        return self.stack[-1]
+    def minimum(self):
+        if not self.minstack:
+            return 0
+        return self.minstack[-1]
+
+obj = Solution()
+obj.push(-2)
+print(obj.stack)
+obj.push(0)
+print(obj.stack)
+obj.push(3)
+print(obj.stack)
+print(obj.minimum())
+obj.pop()
+print(obj.stack)
+print(obj.top())
+print(obj.minimum())
+print(obj.stack)
+
