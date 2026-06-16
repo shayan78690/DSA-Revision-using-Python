26b8919bc7ff9300e7da2fa145c540686b6de9a7
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-17 00:18:52 +0530

    Update 6. stack_using_two_queues.py

--- 10. Stack and Queue/1. Basics and Implementation/6. stack_using_two_queues.py
+++ 10. Stack and Queue/1. Basics and Implementation/6. stack_using_two_queues.py
@@ -1,63 +1,53 @@
 from collections import deque
-
-class MyStack(object):
+class Stack:
     def __init__(self):
-        self.q1 = deque()   # acts like Queue 1
-        self.q2 = deque()   # acts like Queue 2
-
-    def push(self, x):
-        if self.q1:
-            self.q1.append(x)
+        self.queue1 = deque()
+        self.queue2 = deque()
+    def push(self, val):
+        if self.queue1:
+            self.queue1.append(val)
         else:
-            self.q2.append(x)
-
+            self.queue2.append(val)
     def pop(self):
-        if self.empty():
+        if self.isEmpty():
             return -1
-
         top = -1
-        if self.q1:
-            while len(self.q1) > 1:
-                top = self.q1.popleft()
-                self.q2.append(top)
-            top = self.q1.popleft()
+        if self.queue1:
+            while len(self.queue1) > 1:
+                top = self.queue1.popleft()
+                self.queue2.append(top)
+            top = self.queue1.popleft()
         else:
-            while len(self.q2) > 1:
-                top = self.q2.popleft()
-                self.q1.append(top)
-            top = self.q2.popleft()
-
+            while len(self.queue2) > 1:
+                top = self.queue2.popleft()
+                self.queue1.append(top)
+            top = self.queue2.popleft()
         return top
-
-    def top(self):
-        if self.empty():
+        
+    def peek(self):
+        if self.isEmpty():
             return -1
-
         top = -1
-        if self.q1:
-            while len(self.q1) > 1:
-                top = self.q1.popleft()
-                self.q2.append(top)
-            top = self.q1[0]
-            self.q2.append(self.q1.popleft())
+        if self.queue1:
+            while len(self.queue1) > 1:
+                top = self.queue1.popleft()
+                self.queue2.append(top)
+            top = self.queue1[0]
         else:
-            while len(self.q2) > 1:
-                top = self.q2.popleft()
-                self.q1.append(top)
-            top = self.q2[0]
-            self.q1.append(self.q2.popleft())
-
+            while len(self.queue2) > 1:
+                top = self.queue2.popleft()
+                self.queue1.append(top)
+            top = self.queue2[0]
         return top
+    
+    def isEmpty(self):
+        return len(self.queue1) == 0 and len(self.queue2) == 0
 
-    def empty(self):
-        return not self.q1 and not self.q2
-
-
-# Example usage (same as Java comments)
-if __name__ == "__main__":
-    obj = MyStack()
-    obj.push(10)
-    obj.push(20)
-    print(obj.top())    # Output: 20
-    print(obj.pop())    # Output: 20
-    print(obj.empty())  # Output: False
+stack = Stack()
+stack.push(5)
+stack.push(6)
+stack.push(10)
+print(stack.pop())
+print(stack.pop())
+print(stack.peek())
+print(stack.isEmpty())
