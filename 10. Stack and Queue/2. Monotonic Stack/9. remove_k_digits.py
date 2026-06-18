e8a05d7602264d4bea612dd1839ae658fe1386af
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-18 18:24:58 +0530

    Update 9. remove_k_digits.py

--- 10. Stack and Queue/2. Monotonic Stack/9. remove_k_digits.py
+++ 10. Stack and Queue/2. Monotonic Stack/9. remove_k_digits.py
@@ -1,29 +1,15 @@
 class Solution(object):
     def removeKdigits(self, num, k):
-        """
-        :type num: str
-        :type k: int
-        :rtype: str
-        """
         stack = []
-        n = len(num)
-
-        for i in range(n):
-            while stack and k > 0 and stack[-1] > num[i]:
+        for x in num:
+            while stack and k > 0 and stack[-1] > x:
                 stack.pop()
                 k -= 1
-            stack.append(num[i])
-
-        # Remove remaining digits if k > 0
+            stack.append(x)
         while stack and k > 0:
             stack.pop()
             k -= 1
-
-        # Build the result
-        res = ''.join(stack)
-
-        # Remove leading zeros
-        res = res.lstrip('0')
-
-        # Return result or "0" if empty
+        
+        res = "".join(stack)
+        res = res.lstrip("0")
         return res if res else "0"
