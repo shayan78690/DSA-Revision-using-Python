4c8c4c2d6460ff3aaaf82c155b92afb07077642a
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-18 16:31:24 +0530

    Update and rename next_smaller_element.py to 4. next_smaller_element.py

---
+++ 10. Stack and Queue/2. Monotonic Stack/4. next_smaller_element.py
@@ -0,0 +1,14 @@
+class Solution:
+	def nextSmallerEle(self, arr):
+	    n = len(arr)
+	    result = [0] * n
+	    stack = []
+	    for i in range(n-1, -1, -1):
+	        while stack and stack[-1] >= arr[i]:
+	            stack.pop()
+	        if stack:
+	            result[i] = stack[-1]
+	        else:
+	            result[i] = -1
+	        stack.append(arr[i])
+	    return result
