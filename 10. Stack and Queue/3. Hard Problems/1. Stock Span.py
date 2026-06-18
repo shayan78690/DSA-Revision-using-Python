70387632a56bddc59947faf38b8070686f6e006d
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-18 19:21:46 +0530

    Update 1. Stock Span.py

--- 10. Stack and Queue/3. Hard Problems/1. Stock Span.py
+++ 10. Stack and Queue/3. Hard Problems/1. Stock Span.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def calculateSpan(self, arr):
+        n = len(arr)
+        stack = []
+        result = [0] * n
+        for i in range(n):
+            while stack and arr[stack[-1]] <= arr[i]:
+                stack.pop()
+            if not stack:
+                result[i] = i+1
+            else:
+                result[i] = i-stack[-1]
+            stack.append(i)
+        return result
+        
