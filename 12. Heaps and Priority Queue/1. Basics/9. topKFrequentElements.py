652b006e3e3f5c9b7207d34d7ed4362e0840f30d
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-20 16:47:19 +0530

    Update 9. topKFrequentElements.py

--- 12. Heaps and Priority Queue/1. Basics/9. topKFrequentElements.py
+++ 12. Heaps and Priority Queue/1. Basics/9. topKFrequentElements.py
@@ -28,37 +28,32 @@ class Solution(object):
 
 
 import heapq
-from collections import defaultdict
 
 class Pair:
     def __init__(self, element, freq):
         self.element = element
         self.freq = freq
-
+    
     def __lt__(self, other):
         return self.freq < other.freq
 
-
 class Solution(object):
     def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        freq_map = defaultdict(int)
-        for x in nums:
-            freq_map[x] += 1
-
         pq = []
-
-        for element, freq in freq_map.items():
-            heapq.heappush(pq, Pair(element, freq))
+        hashmap = {}
+        for num in nums:
+            hashmap[num] = hashmap.get(num, 0) + 1
+        
+        for key, value in hashmap.items():
+            heapq.heappush(pq, Pair(key, value))
             if len(pq) > k:
                 heapq.heappop(pq)
-
+        
         result = []
         while pq:
-            result.append(heapq.heappop(pq).element)
+            temp = heapq.heappop(pq)
+            result.append(temp.element)
+        return result
 
-        return result[::-1]
+        
+
