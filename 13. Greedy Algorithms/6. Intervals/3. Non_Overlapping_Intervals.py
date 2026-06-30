d9c57767b9788891f5161c892e48ab8fee47f7e7
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-30 15:23:44 +0530

    Update 3. Non_Overlapping_Intervals.py

--- 13. Greedy Algorithms/6. Intervals/3. Non_Overlapping_Intervals.py
+++ 13. Greedy Algorithms/6. Intervals/3. Non_Overlapping_Intervals.py
@@ -5,12 +5,11 @@ class Solution(object):
         :rtype: int
         """
         intervals.sort(key = lambda x : x[1])
-        count = 0
-        lastEnd = intervals[0][1]
-        for i in range(1, len(intervals)):
-            if intervals[i][0] < lastEnd:
-                count += 1
-            else:
-                lastEnd = intervals[i][1]
-        return count
+        maxintervals = 0
+        lastend = float('-inf')
+        for start, end in intervals:
+            if start >= lastend:
+                maxintervals += 1
+                lastend = end
+        return len(intervals)-maxintervals
         
