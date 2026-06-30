401bb3c76be6cafaf98512c6c0ccabd66b5fb3e9
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-06-30 16:37:34 +0530

    Update 1. Merge_Intervals.py

--- 13. Greedy Algorithms/6. Intervals/1. Merge_Intervals.py
+++ 13. Greedy Algorithms/6. Intervals/1. Merge_Intervals.py
@@ -35,4 +35,21 @@ class Solution(object):
             else:
                 ans[-1][1] = max(ans[-1][1], intervals[i][1])
         return ans
-                
+
+
+
+class Solution(object):
+    def merge(self, intervals):
+        n = len(intervals)
+        intervals.sort()
+        result = []
+        result.append(intervals[0])
+        for i in range(1, n):
+            start = intervals[i][0]
+            end = intervals[i][1]
+            if start <= result[-1][1]:
+                result[-1][0] = min(result[-1][0], start)
+                result[-1][1] = max(result[-1][1], end)
+            else:
+                result.append([start, end])
+        return result
