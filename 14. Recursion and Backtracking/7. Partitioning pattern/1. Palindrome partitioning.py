b4fe0c2a64b6c1013467aff3316d6a9f59539c2d
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-05 19:01:09 +0530

    Update 1. Palindrome partitioning.py

--- 14. Recursion and Backtracking/7. Partitioning pattern/1. Palindrome partitioning.py
+++ 14. Recursion and Backtracking/7. Partitioning pattern/1. Palindrome partitioning.py
@@ -19,3 +19,18 @@ class Solution(object):
     def palindrome(self, string):
         return string == string[::-1]
 
+
+
+def func(s, n, result, current, start):
+    if start == n:
+        result.append(current[:])
+        return
+    for end in range(start, n):
+        current.append(s[start:end+1])
+        func(s, n, result, current, end+1)
+        current.pop()
+
+s = input()
+result = []
+func(s, len(s), result, [], 0)
+print(result)
