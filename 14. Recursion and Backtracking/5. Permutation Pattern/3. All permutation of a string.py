d7a324eeb37a1875833a48935fb1cd860dd10e5e
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-05 15:54:03 +0530

    Update 3. All permutation of a string.py

--- 14. Recursion and Backtracking/5. Permutation Pattern/3. All permutation of a string.py
+++ 14. Recursion and Backtracking/5. Permutation Pattern/3. All permutation of a string.py
@@ -1,1 +1,22 @@
-
+class Solution:
+    def permutation(self, s):
+        n = len(s)
+        result = []
+        visited = [False] * n
+        self.func(s, n, result, [], visited)
+        return result
+    
+    def func(self, s, n, result, current, visited):
+        if len(current) == n:
+            string = "".join(current)
+            result.append(string)
+            return
+        for i in range(n):
+            if visited[i]:
+                continue
+            visited[i] = True
+            current.append(s[i])
+            self.func(s, n, result, current, visited)
+            visited[i] = False
+            current.pop()
+            
