f37ea7d3b24b65f01896a166c3f623af43eec10f
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-05 16:49:25 +0530

    Update 1. Combination sum 1.py

--- 14. Recursion and Backtracking/6. Combination pattern/1. Combination sum 1.py
+++ 14. Recursion and Backtracking/6. Combination pattern/1. Combination sum 1.py
@@ -1,1 +1,19 @@
- 
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        n = len(candidates)
+        result = []
+        self.func(candidates, n, target, result, [], 0)
+        return result
+    
+    def func(self, nums, n, target, result, current, idx):
+        if target == 0:
+            result.append(current[:])
+            return
+        if idx == n or target < 0:
+            return
+        current.append(nums[idx])
+        self.func(nums, n, target-nums[idx], result, current, idx)
+        current.pop()
+        self.func(nums, n, target, result, current, idx+1)
+
+         
