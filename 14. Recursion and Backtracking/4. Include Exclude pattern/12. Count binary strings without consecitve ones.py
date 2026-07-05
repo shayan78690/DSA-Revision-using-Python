53a4b1bbe46da43688d83c0c890123b3c0adc961
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-05 15:04:54 +0530

    Update 12. Count binary strings without consecitve ones.py

--- 14. Recursion and Backtracking/4. Include Exclude pattern/12. Count binary strings without consecitve ones.py
+++ 14. Recursion and Backtracking/4. Include Exclude pattern/12. Count binary strings without consecitve ones.py
@@ -9,3 +9,22 @@ class Solution:
         if not string or string[-1] != "1":
             second = self.func(n, string+"1", idx+1)
         return first+second
+
+
+
+class Solution:
+    def countStrings(self, n):
+        return self.func(n, 0, False)
+
+    def func(self, n, idx, prevOne):
+        if idx == n:
+            return 1
+
+        # Always place 0
+        count = self.func(n, idx + 1, False)
+
+        # Place 1 only if previous wasn't 1
+        if not prevOne:
+            count += self.func(n, idx + 1, True)
+
+        return count
