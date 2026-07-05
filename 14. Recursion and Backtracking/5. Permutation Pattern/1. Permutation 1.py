cfaabe8965b004be46322bc886809dfdd0ccb9c3
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-05 15:46:41 +0530

    Update 1. Permutation 1.py

--- 14. Recursion and Backtracking/5. Permutation Pattern/1. Permutation 1.py
+++ 14. Recursion and Backtracking/5. Permutation Pattern/1. Permutation 1.py
@@ -1,1 +1,21 @@
- 
+class Solution(object):
+    def permute(self, nums):
+        n = len(nums)
+        result = []
+        visited = [False] * n
+        self.func(nums, n, result, [], visited)
+        return result
+    
+    def func(self, nums, n, result, current, visited):
+        if len(current) == n:
+            result.append(current[:])
+            return   
+        for i in range(n):
+            if visited[i]:
+                continue
+            visited[i] = True
+            current.append(nums[i])
+            self.func(nums, n, result, current, visited)
+            visited[i] = False
+            current.pop()      
+
