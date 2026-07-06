2f6b2a1fa3f9c00457a478a05abab44289f97cba
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-06 17:14:32 +0530

    Update 4. Word break 1.py

--- 14. Recursion and Backtracking/7. Partitioning pattern/4. Word break 1.py
+++ 14. Recursion and Backtracking/7. Partitioning pattern/4. Word break 1.py
@@ -14,3 +14,26 @@ class Solution(object):
                     return True
         return False
         
+
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        n = len(s)
+        hashset = set(wordDict)
+        dp = [-1] * n
+        return self.func(s, n, hashset, 0, dp)
+    
+    def func(self, s, n, hashset, start, dp):
+        if start == n:
+            return True
+        if dp[start] != -1:
+            return dp[start] == 1
+        for end in range(start, n):
+            part = s[start:end+1]
+            if part in hashset:
+                if self.func(s, n, hashset, end+1, dp):
+                    dp[start] = 1
+                    return True
+        dp[start] = 0
+        return False
+        
