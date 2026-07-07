95b2530d39da4dce3a8e0cc547a553584745ec7a
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-07 16:39:55 +0530

    Create 1. matrix transpose.py

---
+++ 16. Matrix Pattern/4. Matrix transpose/1. matrix transpose.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def transpose(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        answer = [[0] * rows for _ in range(cols)]
+
+        for i in range(rows):
+            for j in range(cols):
+                answer[j][i] = matrix[i][j]
+
+        return answer
+
+
+
