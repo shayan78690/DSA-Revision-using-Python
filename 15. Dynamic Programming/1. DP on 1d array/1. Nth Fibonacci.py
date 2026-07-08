b7c40f0d47088181f681226717d9a378fe42762a
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-08 14:28:02 +0530

    Update 1. Nth Fibonacci.py

--- 15. Dynamic Programming/1. DP on 1d array/1. Nth Fibonacci.py
+++ 15. Dynamic Programming/1. DP on 1d array/1. Nth Fibonacci.py
@@ -8,18 +8,21 @@ class Solution(object):
 class Solution(object):
     def fib(self, n):
         dp = [-1] * (n+1)
+        return self.func(n, dp)
+    
+    def func(self, n, dp):
         if n <= 1:
             return n
         if dp[n] != -1:
             return dp[n]
-        dp[n] = self.fib(n-1) + self.fib(n-2)
+        dp[n] = self.func(n-1, dp) + self.func(n-2, dp)
         return dp[n]
-
+    
 class Solution(object):
     def fib(self, n):
-        dp = [-1] * (n+1)
         if n <= 1:
             return n
+        dp = [-1] * (n+1)
         dp[0] = 0
         dp[1] = 1
         for i in range(2, n+1):
