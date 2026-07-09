57dad3dfe8d04ff62739f1dd0a3abfa46196450a
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-09 15:37:12 +0530

    Create 7. Ninja Training.py

---
+++ 15. Dynamic Programming/3. 2d dp | Dp on grids/7. Ninja Training.py
@@ -0,0 +1,81 @@
+class Solution:
+    def maximumPoints(self, mat):
+        n = len(mat)
+        return self.func(mat, n, 0, 3)
+    
+    def func(self, mat, n, day, last):
+        if day == n:
+            return 0
+        
+        first = 0
+        if last != 0:
+            first = max(first, mat[day][0] + self.func(mat, n, day+1, 0))
+        
+        second = 0
+        if last != 1:
+            second = max(second, mat[day][1] + self.func(mat, n, day+1, 1))
+        
+        third = 0
+        if last != 2:
+            third = max(third, mat[day][2] + self.func(mat, n, day+1, 2))
+        
+        return max(first, second, third)
+        
+
+
+
+
+
+
+
+
+class Solution:
+    def maximumPoints(self, mat):
+        n = len(mat)
+        dp = [[-1] * 4 for _ in range(n)]
+        return self.func(mat, n, 0, 3, dp)
+    
+    def func(self, mat, n, day, last, dp):
+        if day == n:
+            return 0
+        if dp[day][last] != -1:
+            return dp[day][last]
+        
+        first = 0
+        if last != 0:
+            first = max(first, mat[day][0] + self.func(mat, n, day+1, 0, dp))
+        
+        second = 0
+        if last != 1:
+            second = max(second, mat[day][1] + self.func(mat, n, day+1, 1, dp))
+        
+        third = 0
+        if last != 2:
+            third = max(third, mat[day][2] + self.func(mat, n, day+1, 2, dp))
+        
+        dp[day][last] = max(first, second, third)
+        return dp[day][last]
+
+
+class Solution:
+    def maximumPoints(self, mat):
+        n = len(mat)
+        dp = [[0] * 4 for _ in range(n+1)]
+        
+        for day in range(n-1, -1, -1):
+            for last in range(4):
+                first = 0
+                if last != 0:
+                    first = max(first, mat[day][0] + dp[day+1][0])
+        
+                second = 0
+                if last != 1:
+                    second = max(second, mat[day][1] + dp[day+1][1])
+        
+                third = 0
+                if last != 2:
+                    third = max(third, mat[day][2] + dp[day+1][2])
+        
+                dp[day][last] = max(first, second, third)
+        
+        return dp[0][3]
