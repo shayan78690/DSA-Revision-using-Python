adb0ada5dddd4c6d77d1c2cac15f1d15275a7e36
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-09 15:43:51 +0530

    Update 4. Triangle.py

--- 15. Dynamic Programming/3. 2d dp | Dp on grids/4. Triangle.py
+++ 15. Dynamic Programming/3. 2d dp | Dp on grids/4. Triangle.py
@@ -42,3 +42,20 @@ class Solution(object):
     
  
         
+
+
+class Solution(object):
+    def minimumTotal(self, triangle):
+        n = len(triangle)
+        dp = [[0] * n for _ in range(n)]
+        for j in range(n):
+            dp[n-1][j] = triangle[n-1][j]
+        for i in range(n-2, -1, -1):
+            for j in range(i, -1, -1):
+                down = triangle[i][j] + dp[i+1][j]
+                diagonal = triangle[i][j] + dp[i+1][j+1]
+                dp[i][j] = min(down, diagonal)
+        return dp[0][0]
+    
+ 
+        
