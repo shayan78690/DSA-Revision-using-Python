a5c9ccc494ab6a0458f59ca70938c9d4c4d717d2
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-10 20:41:15 +0530

    Update 2. Print longest common subsequence.py

--- 15. Dynamic Programming/6. Dp on string/2. Print longest common subsequence.py
+++ 15. Dynamic Programming/6. Dp on string/2. Print longest common subsequence.py
@@ -67,3 +67,42 @@ class Solution:
                         dp[i][j] = dp[i][j + 1]
 
         return dp[0][0]
+
+
+
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1, text2):
+        n = len(text1)
+        m = len(text2)
+
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+        ans = []
+
+        i = 0
+        j = 0
+
+        while i < n and j < m:
+
+            if text1[i] == text2[j]:
+                ans.append(text1[i])
+                i += 1
+                j += 1
+
+            elif dp[i + 1][j] >= dp[i][j + 1]:
+                i += 1
+
+            else:
+                j += 1
+
+        return "".join(ans)
