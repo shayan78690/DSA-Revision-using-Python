6f569a8d7c13f4ffb87ab8615dff0c2ee21ac3d6
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-11 21:40:04 +0530

    Update 5. Uncrossed lines.py

--- 15. Dynamic Programming/7. Counting DP/5. Uncrossed lines.py
+++ 15. Dynamic Programming/7. Counting DP/5. Uncrossed lines.py
@@ -18,3 +18,19 @@ class Solution(object):
         return dp[i][j]
 
         
+
+
+class Solution(object):
+    def maxUncrossedLines(self, nums1, nums2):
+        n, m = len(nums1), len(nums2)
+
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                if nums1[i] == nums2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+        return dp[0][0]
