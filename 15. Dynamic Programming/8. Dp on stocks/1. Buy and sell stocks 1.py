f086ec065a02a76b2c354a294d5ced141faf47db
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-12 15:19:49 +0530

    Update 1. Buy and sell stocks 1.py

--- 15. Dynamic Programming/8. Dp on stocks/1. Buy and sell stocks 1.py
+++ 15. Dynamic Programming/8. Dp on stocks/1. Buy and sell stocks 1.py
@@ -1,1 +1,11 @@
-
+class Solution(object):
+    def maxProfit(self, prices):
+        n = len(prices)
+        buy_price = prices[0]
+        maxi = 0
+        for i in range(1, n):
+            profit = prices[i] - buy_price
+            maxi = max(maxi, profit)
+            buy_price = min(buy_price, prices[i])
+        return maxi
+        
