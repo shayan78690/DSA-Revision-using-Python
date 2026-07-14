66840d22dcd35e38d49f77703530b9af1e906c0c
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-14 14:46:51 +0530

    Update 2. Print longest increasing subsequence.py

--- 15. Dynamic Programming/9. LIS/2. Print longest increasing subsequence.py
+++ 15. Dynamic Programming/9. LIS/2. Print longest increasing subsequence.py
@@ -1,28 +1,25 @@
 class Solution:
     def getLIS(self, arr):
         n = len(arr)
-        dp = [[0] * (n+1) for _ in range(n+1)]
-        for index in range(n-1, -1, -1):
-            for prevIndex in range(index-1, -2, -1):
-                skip = dp[index+1][prevIndex+1]
-                take = 0
-                if prevIndex == -1 or arr[index] > arr[prevIndex]:
-                    take = 1 + dp[index+1][index+1]
-                dp[index][prevIndex+1] = max(take, skip)
-        
-        index = 0
-        prevIndex = -1
-        result = []
-        while index < n:
-            skip = dp[index+1][prevIndex+1]
-            take = 0
-            if prevIndex == -1 or arr[index] > arr[prevIndex]:
-                take = 1 + dp[index+1][index+1]
+        dp = [1] * n
+        parent = [i for i in range(n)]
+        maxlen = 1
+        lastIndex = 0
+        for index in range(n):
+            for prevIndex in range(index):
+                if arr[index] > arr[prevIndex]:
+                    if dp[prevIndex] + 1 > dp[index]:
+                        dp[index] = dp[prevIndex] + 1
+                        parent[index] = prevIndex
+            if dp[index] > maxlen:
+                maxlen = dp[index]
+                lastIndex = index
             
-            if take >= skip and (prevIndex == -1 or arr[index] > arr[prevIndex]):
-                result.append(arr[index])
-                prevIndex = index
-                
-            index += 1
-        
+        result = []
+        while parent[lastIndex] != lastIndex:
+            result.append(arr[lastIndex])
+            lastIndex = parent[lastIndex]
+            
+        result.append(arr[lastIndex])
+        result.reverse()
         return result
