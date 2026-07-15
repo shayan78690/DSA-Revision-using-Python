15d1c87b91dc878be0aa4b28c632844d0d2d9dc0
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-15 18:21:36 +0530

    Update and rename 1. test.py to 1. Matrix chain multiplication.py

---
+++ 15. Dynamic Programming/10. Partion DP/1. Matrix chain multiplication.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def matrixMultiplication(self, arr):
+        n = len(arr)
+        dp = [[-1] * n for _ in range(n)]
+        return self.func(arr, 1, n-1, dp)
+        
+    def func(self, arr, i, j, dp):
+        if i == j:
+            return 0
+        if dp[i][j] != -1:
+            return dp[i][j]
+        ans = float('inf')
+        for k in range(i, j):
+            cost = (self.func(arr, i, k, dp) + self.func(arr, k+1, j, dp) + arr[i-1] * arr[k] * arr[j])
+            ans = min(ans, cost)
+        dp[i][j] = ans
+        return dp[i][j]
