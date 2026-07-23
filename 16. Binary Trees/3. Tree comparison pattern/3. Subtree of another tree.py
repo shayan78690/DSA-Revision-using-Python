2bec98eb88e5e9bb71e3737fc786e47160053302
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-23 21:25:48 +0530

    Update 3. Subtree of another tree.py

--- 16. Binary Trees/3. Tree comparison pattern/3. Subtree of another tree.py
+++ 16. Binary Trees/3. Tree comparison pattern/3. Subtree of another tree.py
@@ -1,16 +1,12 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def isSubtree(self, root, subRoot):
         if not root:
             return False
+
         if self.isSame(root, subRoot):
             return True
-        return self.isSame(root.left, subRoot) or self.isSame(root.right, subRoot)
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
     def isSame(self, p, q):
         if not p and not q:
@@ -19,5 +15,5 @@ class Solution(object):
             return False
         if p.val != q.val:
             return False
+
         return self.isSame(p.left, q.left) and self.isSame(p.right, q.right)
-        
