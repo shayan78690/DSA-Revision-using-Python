2115ee58718bc56b15f83c3d1a5341b529bdf571
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-23 19:15:24 +0530

    Create 4. Iterative preorder traversal.py

---
+++ 16. Binary Trees/1. Basic DFS traversal/4. Iterative preorder traversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        if root is None:
+            return []
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return result
+        
