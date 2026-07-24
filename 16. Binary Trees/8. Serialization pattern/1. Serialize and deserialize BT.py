e1b35673f21fcc920667144671b1d76878886d25
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-25 01:19:22 +0530

    Update 1. Serialize and deserialize BT.py

--- 16. Binary Trees/8. Serialization pattern/1. Serialize and deserialize BT.py
+++ 16. Binary Trees/8. Serialization pattern/1. Serialize and deserialize BT.py
@@ -1,1 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Codec:
+
+    def dfs(self, root, result):
+        if not root:
+            result.append("N")
+            return
+        result.append(str(root.val))
+        self.dfs(root.left, result)
+        self.dfs(root.right, result)
+    
+    def func(self, values):
+        if values[self.index] == "N":
+            self.index += 1
+            return None
+        node = TreeNode(int(values[self.index]))
+        self.index += 1
+        node.left = self.func(values)
+        node.right = self.func(values)
+        return node
+
+    def serialize(self, root):
+        result = []
+        self.dfs(root, result)
+        return ",".join(result)
+        
+
+    def deserialize(self, data):
+        values = data.split(",")
+        self.index = 0
+        return self.func(values)
+        
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
