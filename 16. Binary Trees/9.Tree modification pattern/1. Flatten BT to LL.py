a0017f211c22a257ffacd0eab03020b1f0f9363d
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-25 01:30:28 +0530

    Update 1. Flatten BT to LL.py

--- 16. Binary Trees/9.Tree modification pattern/1. Flatten BT to LL.py
+++ 16. Binary Trees/9.Tree modification pattern/1. Flatten BT to LL.py
@@ -1,1 +1,22 @@
-
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def flatten(self, root):
+        if not root:
+            return
+        stack = [root]
+        prev = None
+        while stack:
+            node = stack.pop()
+            if prev:
+                prev.right = node
+                prev.left = None
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+            prev = node
