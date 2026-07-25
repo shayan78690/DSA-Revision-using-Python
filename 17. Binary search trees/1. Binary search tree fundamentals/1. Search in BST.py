31573b9f50dbcd8cdd96d6696b3ae58bcf91af3a
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-25 18:28:52 +0530

    Update 1. Search in BST.py

--- 17. Binary search trees/1. Binary search tree fundamentals/1. Search in BST.py
+++ 17. Binary search trees/1. Binary search tree fundamentals/1. Search in BST.py
@@ -1,1 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        if not root:
+            return None
+        while root:
+            if root.val == val:
+                return root
+            elif root.val > val:
+                root = root.left
+            else:
+                root = root.right
+        return None
 
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        if val < root.val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
