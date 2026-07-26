00f5491163b4109e7114ff4534ce4af0a560772a
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-26 19:16:59 +0530

    Update 1. 01 Matrix.py

--- 18. Graph/2. DFS | BFS pattern/2. Multisource BFS/1. 01 Matrix.py
+++ 18. Graph/2. DFS | BFS pattern/2. Multisource BFS/1. 01 Matrix.py
@@ -22,3 +22,28 @@ class Solution(object):
                     q.append((new_row, new_col))
         return dis
         
+from collections import deque
+
+class Solution(object):
+    def updateMatrix(self, mat):
+        rows, cols = len(mat), len(mat[0])
+        dis = [[-1] * cols for _ in range(rows)]
+        q = deque()
+        for i in range(rows):
+            for j in range(cols):
+                if mat[i][j] == 0:
+                    dis[i][j] = 0
+                    q.append((i, j)) 
+        
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        while q:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    new_row = dr + r
+                    new_col = dc + c
+                    if 0 <= new_row < rows and 0 <= new_col < cols and dis[new_row][new_col] == -1:
+                        dis[new_row][new_col] = dis[r][c] + 1
+                        q.append((new_row, new_col))
+        return dis
+        
