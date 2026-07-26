5d1599f94fb92a35d2633f3fa3dd18340c9dfb61
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-26 13:54:36 +0530

    Update 1. Number of provinces.py

--- 18. Graph/2. DFS | BFS pattern/1. Count components/1. Number of provinces.py
+++ 18. Graph/2. DFS | BFS pattern/1. Count components/1. Number of provinces.py
@@ -32,3 +32,41 @@ class Solution(object):
         return count
     
         
+
+from collections import deque
+
+class Solution(object):
+
+    def dfs(self, node, visited, adj_list):
+        visited[node] = True
+        for neighbour in adj_list[node]:
+            if not visited[neighbour]:
+                self.dfs(neighbour, visited, adj_list)
+    
+    def bfs(self, node, visited, adj_list):
+        q = deque()
+        q.append(node)
+        visited[node] = True
+        while q:
+            temp = q.popleft()
+            for neighbour in adj_list[temp]:
+                if not visited[neighbour]:
+                    visited[neighbour] = True
+                    q.append(neighbour)
+
+    def findCircleNum(self, isConnected):
+        V = len(isConnected)
+        adj_list = [[] for _ in range(V)]
+        for i in range(V):
+            for j in range(V):
+                if isConnected[i][j] == 1 and i != j:
+                    adj_list[i].append(j)
+                    adj_list[j].append(i)
+        visited = [False] * V
+        count = 0
+        for i in range(V):
+            if not visited[i]:
+                self.bfs(i, visited, adj_list)
+                count += 1
+        return count
+        
