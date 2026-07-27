8c9c89fc864ef1501653d4c470149318828b0c0e
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-27 20:08:20 +0530

    Create 2. Course schedule 2.py

---
+++ 18. Graph/3. Topological sorting/4. Course Schedule/2. Course schedule 2.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class Solution:
+    def findOrder(self, numCourses, prerequisites):
+        adj = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for course, pre in prerequisites:
+            adj[pre].append(course)
+            indegree[course] += 1
+
+        queue = deque()
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        order = []
+
+        while queue:
+            node = queue.popleft()
+            order.append(node)
+
+            for neighbor in adj[node]:
+                indegree[neighbor] -= 1
+
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        if len(order) == numCourses:
+            return order
+
+        return []
