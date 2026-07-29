27f343cab89bbed2532aaa032bff3b73c282cc69
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-29 19:40:40 +0530

    Create 5. Find Minimum Time to Reach Last Room II.py

---
+++ 18. Graph/4. Shortest path pattern/4. Grid Dijkstra/5. Find Minimum Time to Reach Last Room II.py
@@ -0,0 +1,26 @@
+import heapq
+class Solution(object):
+    def minTimeToReach(self, moveTime):
+        n, m = len(moveTime), len(moveTime[0])
+        dist = [[float('inf')] * m for _ in range(n)]
+        dist[0][0] = 0
+        minheap = []
+        heapq.heappush(minheap, (0, 0, 0, 1))
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        while minheap:
+            time, r, c, cost = heapq.heappop(minheap)
+            if r == n-1 and c == m-1:
+                return dist[r][c]
+            if time > dist[r][c]:
+                continue
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+                if 0 <= nr < n and 0 <= nc < m:
+                    moveCost = cost
+                    arrival = max(time, moveTime[nr][nc]) + moveCost
+                    newCost = 2 if moveCost == 1 else 1
+                    if arrival < dist[nr][nc]:
+                        dist[nr][nc] = arrival
+                        heapq.heappush(minheap, (arrival, nr, nc, newCost))
+        return -1
