fa017ba30a68b5d774ed8d948ba9508512773fa8
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-07-31 20:24:41 +0530

    Create 8. Island Perimeter.py

---
+++ 18. Graph/2. DFS | BFS pattern/5. Island pattern/8. Island Perimeter.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        rows = len(grid)
+        cols = len(grid[0])
+        perimeter = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    perimeter += 4
+
+                    if r > 0 and grid[r - 1][c] == 1:
+                        perimeter -= 1
+
+                    if r < rows - 1 and grid[r + 1][c] == 1:
+                        perimeter -= 1
+
+                    if c > 0 and grid[r][c - 1] == 1:
+                        perimeter -= 1
+
+                    if c < cols - 1 and grid[r][c + 1] == 1:
+                        perimeter -= 1
+
+        return perimeter
