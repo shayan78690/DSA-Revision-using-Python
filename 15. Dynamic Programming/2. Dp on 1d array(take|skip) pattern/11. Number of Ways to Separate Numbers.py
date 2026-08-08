e44c15a39e40045a5927e7459482603ddb89f080
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-08-08 18:51:38 +0530

    Create 11. Number of Ways to Separate Numbers.py

---
+++ 15. Dynamic Programming/2. Dp on 1d array(take|skip) pattern/11. Number of Ways to Separate Numbers.py
@@ -0,0 +1,109 @@
+class Solution(object):
+    def numberOfCombinations(self, num):
+        n = len(num)
+        MOD = 10**9 + 7
+
+        dp = [[-1] * (n + 1) for _ in range(n + 1)]
+
+        return self.solve(num, n, 0, "", MOD, dp)
+
+    def solve(self, s, n, index, prev, MOD, dp):
+
+        if index == n:
+            return 1
+
+        if s[index] == "0":
+            return 0
+
+        if prev == "":
+            prev_index = n
+        else:
+            prev_index = index - len(prev)
+
+        if dp[index][prev_index] != -1:
+            return dp[index][prev_index]
+
+        count = 0
+
+        for end in range(index, n):
+
+            current = s[index:end + 1]
+
+            if prev == "" or self.isValid(current, prev):
+
+                count = (
+                    count +
+                    self.solve(
+                        s,
+                        n,
+                        end + 1,
+                        current,
+                        MOD,
+                        dp
+                    )
+                ) % MOD
+
+        dp[index][prev_index] = count
+
+        return count
+
+    def isValid(self, current, prev):
+
+        if len(current) > len(prev):
+            return True
+
+        if len(prev) > len(current):
+            return False
+
+        return current >= prev
+
+
+
+class Solution(object):
+    def numberOfCombinations(self, num):
+        n = len(num)
+        MOD = 10**9 + 7
+
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for prev_index in range(n + 1):
+            dp[n][prev_index] = 1
+
+        for index in range(n - 1, -1, -1):
+
+            if num[index] == "0":
+                continue
+
+            for prev_index in range(n + 1):
+
+                if prev_index == n:
+                    prev = ""
+                else:
+                    prev = num[prev_index:index]
+
+                count = 0
+
+                for end in range(index, n):
+
+                    current = num[index:end + 1]
+
+                    if prev == "" or self.isValid(current, prev):
+
+                        count = (
+                            count +
+                            dp[end + 1][index]
+                        ) % MOD
+
+                dp[index][prev_index] = count
+
+        return dp[0][n]
+
+    def isValid(self, current, prev):
+
+        if len(current) > len(prev):
+            return True
+
+        if len(prev) > len(current):
+            return False
+
+        return current >= prev
