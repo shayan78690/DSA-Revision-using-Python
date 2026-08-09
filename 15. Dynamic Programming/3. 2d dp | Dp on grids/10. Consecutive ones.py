8d948a29329d90bd678181bdb60d716093b231a5
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-08-09 17:13:21 +0530

    Update 10. Consecutive ones.py

--- 15. Dynamic Programming/3. 2d dp | Dp on grids/10. Consecutive ones.py
+++ 15. Dynamic Programming/3. 2d dp | Dp on grids/10. Consecutive ones.py
@@ -1,31 +1,35 @@
 class Solution:
     def countStrings(self, n):
         dp = [[-1] * 2 for _ in range(n)]
-        return self.func(n, 0, False, dp)
+        return self.func(n, 0, 0, dp)
 
-    def func(self, n, idx, prevOne, dp):
+    def func(self, n, idx, prev, dp):
         if idx == n:
             return 1
-        if dp[idx][prevOne] != -1:
-            return dp[idx][prevOne]
+        if dp[idx][prev] != -1:
+            return dp[idx][prev]
         count = 0
-        count += self.func(n, idx + 1, False, dp)
+        count += self.func(n, idx + 1, 0, dp)
 
-        if not prevOne:
-            count += self.func(n, idx + 1, True, dp)
+        if prev == 0:
+            count += self.func(n, idx + 1, 1, dp)
         
-        dp[idx][prevOne] = count
-        return dp[idx][prevOne]
+        dp[idx][prev] = count
+        return dp[idx][prev]
 
 
 class Solution:
     def countStrings(self, n):
-        dp = [[0] * 2 for _ in range(n + 1)]
-
-        dp[n][0] = dp[n][1] = 1
-
-        for i in range(n - 1, -1, -1):
-            dp[i][0] = dp[i + 1][0] + dp[i + 1][1]
-            dp[i][1] = dp[i + 1][0]
-
+        dp = [[0] * 2 for _ in range(n+1)]
+        dp[n][0] = 1
+        dp[n][1] = 1
+        for idx in range(n-1, -1, -1):
+            for prev in range(2):
+                count = 0
+                count += dp[idx+1][0]
+                if prev == 0:
+                    count += dp[idx+1][1]
+                dp[idx][prev] = count
         return dp[0][0]
+
+   
