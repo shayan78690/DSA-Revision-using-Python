ce5d70ee19e689afadecf94a1e5034bb1ac9ff09
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-08-09 20:03:58 +0530

    Update 9. Ways to Express an Integer as Sum of Powers.py

--- 15. Dynamic Programming/3. 2d dp | Dp on grids/9. Ways to Express an Integer as Sum of Powers.py
+++ 15. Dynamic Programming/3. 2d dp | Dp on grids/9. Ways to Express an Integer as Sum of Powers.py
@@ -30,4 +30,46 @@ class Solution(object):
         skip = self.func(n, x, number+1, dp)
         dp[n][number] = take+skip
         return dp[n][number]
-        
+
+
+
+class Solution(object):
+    def numberOfWays(self, n, x):
+        MOD = 10**9+7
+        dp = [[-1] * (n+1) for _ in range(n+1)]
+        return self.solve(n, x, 1, dp, MOD)
+    
+    def solve(self, n, x, number, dp, MOD):
+        if n == 0:
+            return 1
+        power = number**x
+        if power > n:
+            return 0
+        if dp[n][number] != -1:
+            return dp[n][number]
+        count = 0
+        count = (count + self.solve(n-power, x, number+1, dp, MOD)) % MOD
+        count = (count + self.solve(n, x, number+1, dp, MOD)) % MOD
+        dp[n][number] = count
+        return dp[n][number]
+
+
+
+class Solution(object):
+    def numberOfWays(self, n, x):
+        MOD = 10**9+7
+        dp = [[0] * (n+2) for _ in range(n+1)]
+        for number in range(n+2):
+            dp[0][number] = 1
+        for remaining in range(1, n+1):
+            for number in range(n, 0, -1):
+                power = number**x
+                if power > remaining:
+                    dp[remaining][number] = 0
+                    continue
+                count = 0
+                count = (count + dp[remaining-power][number+1]) % MOD
+                count = (count + dp[remaining][number+1]) % MOD
+
+                dp[remaining][number] = count
+        return dp[n][1] 
