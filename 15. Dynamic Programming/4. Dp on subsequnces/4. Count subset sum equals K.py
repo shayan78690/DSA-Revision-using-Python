0cf4e734504aac9728f69d1adc9f70481b69054e
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-08-10 17:27:14 +0530

    Update 4. Count subset sum equals K.py

--- 15. Dynamic Programming/4. Dp on subsequnces/4. Count subset sum equals K.py
+++ 15. Dynamic Programming/4. Dp on subsequnces/4. Count subset sum equals K.py
@@ -1,41 +1,33 @@
-from typing import List
-
-def findWays(arr: List[int], k: int) -> int:
-    n = len(arr)
-    mod = 10**9+7
-    dp = [[-1] * (k+1) for _ in range(n)]
-    return func(arr, n, 0, k, dp) % mod
-
-def func(arr, n, index, k, dp):
-    if index == n:
-        return 1 if k == 0 else 0
-
-    if dp[index][k] != -1:
-        return dp[index][k]
-
-    exclude = func(arr, n, index + 1, k, dp)
-
-    include = 0
-    if arr[index] <= k:
-        include = func(arr, n, index + 1, k - arr[index], dp)
-
-    dp[index][k] = include + exclude
-    return dp[index][k]
+class Solution:
+	def perfectSum(self, arr, target):
+		n = len(arr)
+		dp = [[-1] * (target+1) for _ in range(n+1)]
+		return self.solve(arr, n, target, 0, 0, dp)
+	
+	def solve(self, arr, n, target, index, current_sum, dp):
+	    if index == n:
+	        return 1 if current_sum == target else 0
+	    if dp[index][current_sum] != -1:
+	        return dp[index][current_sum]
+	    skip = self.solve(arr, n, target, index+1, current_sum, dp)
+	    take = 0
+	    if arr[index] + current_sum <= target:
+	        take = self.solve(arr, n, target, index+1, arr[index]+current_sum, dp)
+	        
+	    dp[index][current_sum] = take + skip
+	    return dp[index][current_sum]
 
 
-from typing import List
-
-def findWays(arr: List[int], k: int) -> int:
-    n = len(arr)
-    mod = 10**9+7
-    dp = [[0] * (k+1) for _ in range(n+1)]
-    dp[n][0] = 1
-    for index in range(n-1, -1, -1):
-        for target in range(k+1):
-            exclude = dp[index+1][target]
-            include = 0
-            if arr[index] <= target:
-                include = dp[index+1][target-arr[index]]
-            dp[index][target] = include + exclude
-    return dp[0][target] % mod
-
+class Solution:
+	def perfectSum(self, arr, target):
+		n = len(arr)
+		dp = [[0] * (target+1) for _ in range(n+1)]
+		dp[n][target] = 1
+		for index in range(n-1, -1, -1):
+		    for current_sum in range(target+1):
+		        count = dp[index+1][current_sum]
+		        if arr[index] + current_sum <= target:
+		            count += dp[index+1][current_sum+arr[index]]
+		        dp[index][current_sum] = count
+		return dp[0][0]
+	
