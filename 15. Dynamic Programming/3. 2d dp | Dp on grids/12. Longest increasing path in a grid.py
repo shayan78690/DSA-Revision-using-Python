481fa8b2e30bfded784cbb47b821755ec52a9098
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-08-10 10:13:16 +0530

    Create 12. Longest increasing path in a grid.py

---
+++ 15. Dynamic Programming/3. 2d dp | Dp on grids/12. Longest increasing path in a grid.py
@@ -0,0 +1,85 @@
+class Solution:
+    def longestIncreasingPath(self, matrix):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        ans = 0
+
+        for r in range(m):
+            for c in range(n):
+                ans = max(ans, self.solve(matrix, r, c))
+
+        return ans
+
+    def solve(self, matrix, r, c):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        ans = 1
+
+        directions = [
+            (1, 0),
+            (-1, 0),
+            (0, 1),
+            (0, -1)
+        ]
+
+        for dr, dc in directions:
+            nr = r + dr
+            nc = c + dc
+
+            if 0 <= nr < m and 0 <= nc < n:
+                if matrix[nr][nc] > matrix[r][c]:
+                    ans = max(
+                        ans,
+                        1 + self.solve(matrix, nr, nc)
+                    )
+
+        return ans
+
+
+class Solution:
+    def longestIncreasingPath(self, matrix):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        dp = [[-1] * n for _ in range(m)]
+
+        ans = 0
+
+        for r in range(m):
+            for c in range(n):
+                ans = max(ans, self.solve(matrix, r, c, dp))
+
+        return ans
+
+    def solve(self, matrix, r, c, dp):
+        if dp[r][c] != -1:
+            return dp[r][c]
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        ans = 1
+
+        directions = [
+            (1, 0),
+            (-1, 0),
+            (0, 1),
+            (0, -1)
+        ]
+
+        for dr, dc in directions:
+            nr = r + dr
+            nc = c + dc
+
+            if 0 <= nr < m and 0 <= nc < n:
+                if matrix[nr][nc] > matrix[r][c]:
+                    ans = max(
+                        ans,
+                        1 + self.solve(matrix, nr, nc, dp)
+                    )
+
+        dp[r][c] = ans
+
+        return ans
