113332c325fd4521716f074ef325a7a61989c829
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-08-11 17:45:39 +0530

    Update 4. Interleaving string.py

--- 15. Dynamic Programming/7. Counting DP/4. Interleaving string.py
+++ 15. Dynamic Programming/7. Counting DP/4. Interleaving string.py
@@ -1,160 +1,51 @@
 class Solution(object):
     def isInterleave(self, s1, s2, s3):
-        if len(s1) + len(s2) != len(s3):
-            return False
-
-        return self.func(s1, s2, s3, 0, 0)
-
-    def func(self, s1, s2, s3, i, j):
-
-        if i == len(s1) and j == len(s2):
-            return True
-
-        k = i + j
-
-        # Condition 1 : Both match
-        if (i < len(s1) and j < len(s2) and
-            s1[i] == s3[k] and s2[j] == s3[k]):
-            return (self.func(s1, s2, s3, i + 1, j) or
-                    self.func(s1, s2, s3, i, j + 1))
-
-        # Condition 2 : Only s1 matches
-        if i < len(s1) and s1[i] == s3[k]:
-            return self.func(s1, s2, s3, i + 1, j)
-
-        # Condition 3 : Only s2 matches
-        if j < len(s2) and s2[j] == s3[k]:
-            return self.func(s1, s2, s3, i, j + 1)
-
-        return False
-
-
-class Solution(object):
-    def isInterleave(self, s1, s2, s3):
-        if len(s1) + len(s2) != len(s3):
-            return False
-        
         n, m = len(s1), len(s2)
-        dp = [[None] * m for _ in range(n)]
-        return self.func(s1, s2, s3, 0, 0, dp)
-
-    def func(self, s1, s2, s3, i, j, dp):
-
-        if i == len(s1) and j == len(s2):
-            return True
-        if dp[i][j] is not None:
-            return dp[i][j]
-
-        k = i + j
-
-        # Condition 1 : Both match
-        if (i < len(s1) and j < len(s2) and
-            s1[i] == s3[k] and s2[j] == s3[k]):
-            dp[i][j] = (self.func(s1, s2, s3, i + 1, j, dp) or
-                    self.func(s1, s2, s3, i, j + 1, dp))
-
-        # Condition 2 : Only s1 matches
-        if i < len(s1) and s1[i] == s3[k]:
-            dp[i][j] = self.func(s1, s2, s3, i + 1, j, dp)
-
-        # Condition 3 : Only s2 matches
-        if j < len(s2) and s2[j] == s3[k]:
-            dp[i][j] = self.func(s1, s2, s3, i, j + 1, dp)
-
-        return dp[i][j]
-
-
-class Solution(object):
-    def isInterleave(self, s1, s2, s3):
-        if len(s1) + len(s2) != len(s3):
+        if (n+m) != len(s3):
             return False
-        
-        n, m = len(s1), len(s2)
         dp = [[None] * (m+1) for _ in range(n+1)]
-        return self.func(s1, s2, s3, 0, 0, dp)
-
-    def func(self, s1, s2, s3, i, j, dp):
-
-        if i == len(s1) and j == len(s2):
-            return True
-        if dp[i][j] is not None:
+        def solve(i, j):
+            if i == n and j == m:
+                return True
+            if dp[i][j] is not None:
+                return dp[i][j]
+            k = i+j
+            if (i < n and s1[i] == s3[k] and j < m and s2[j] == s3[k]):
+                dp[i][j] = solve(i+1, j) or solve(i, j+1)
+            elif i < n and s1[i] == s3[k]:
+                dp[i][j] = solve(i+1, j)
+            elif j < m and s2[j] == s3[k]:
+                dp[i][j] = solve(i, j+1)
+            else:
+                dp[i][j] = False
             return dp[i][j]
-
-        k = i + j
-
-        # Condition 1 : Both match
-        if (i < len(s1) and j < len(s2) and
-            s1[i] == s3[k] and s2[j] == s3[k]):
-            dp[i][j] = (self.func(s1, s2, s3, i + 1, j, dp) or
-                    self.func(s1, s2, s3, i, j + 1, dp))
-
-        # Condition 2 : Only s1 matches
-        if i < len(s1) and s1[i] == s3[k]:
-            dp[i][j] = self.func(s1, s2, s3, i + 1, j, dp)
-
-        # Condition 3 : Only s2 matches
-        if j < len(s2) and s2[j] == s3[k]:
-            dp[i][j] = self.func(s1, s2, s3, i, j + 1, dp)
-
-        return dp[i][j]
-
-  class Solution(object):
-    def isInterleave(self, s1, s2, s3):
-        if len(s1) + len(s2) != len(s3):
-            return False
-        
-        # n, m = len(s1), len(s2)
-        # dp = [[None] * (m+1) for _ in range(n+1)]
-        return self.func(s1, s2, s3, 0, 0)
-
-    def func(self, s1, s2, s3, i, j):
-        if i == len(s1) and j == len(s2):
-            return True
-        
-        k = i+j
-        if i < len(s1) and s1[i] == s3[k]:
-            if self.func(s1, s2, s3, i+1, j):
-                return True
-        
-        if j < len(s2) and s2[j] == s3[k]:
-            if self.func(s1, s2, s3, i, j+1):
-                return True
-        
-        return False
+        return solve(0, 0)
 
 
 
 class Solution(object):
     def isInterleave(self, s1, s2, s3):
-        if len(s1) + len(s2) != len(s3):
+        n, m = len(s1), len(s2)
+
+        if n + m != len(s3):
             return False
 
-        n, m = len(s1), len(s2)
-        dp = [[None] * (m + 1) for _ in range(n + 1)]
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
 
-        return self.func(s1, s2, s3, 0, 0, dp)
+        dp[n][m] = True
 
-    def func(self, s1, s2, s3, i, j, dp):
+        for i in range(n, -1, -1):
+            for j in range(m, -1, -1):
 
-        if i == len(s1) and j == len(s2):
-            return True
+                if i == n and j == m:
+                    continue
 
-        if dp[i][j] is not None:
-            return dp[i][j]
+                k = i + j
 
-        k = i + j
+                if i < n and s1[i] == s3[k]:
+                    dp[i][j] = dp[i][j] or dp[i + 1][j]
 
-        if i < len(s1) and s1[i] == s3[k]:
-            if self.func(s1, s2, s3, i + 1, j, dp):
-                dp[i][j] = True
-                return True
+                if j < m and s2[j] == s3[k]:
+                    dp[i][j] = dp[i][j] or dp[i][j + 1]
 
-        if j < len(s2) and s2[j] == s3[k]:
-            if self.func(s1, s2, s3, i, j + 1, dp):
-                dp[i][j] = True
-                return True
-
-        dp[i][j] = False
-        return False
-
-
+        return dp[0][0]
