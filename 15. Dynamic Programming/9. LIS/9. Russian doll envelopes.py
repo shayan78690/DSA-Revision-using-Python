856ef0d501ad451914d07a278316fc10b67d2ad5
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-08-12 19:36:15 +0530

    Update 9. Russian doll envelopes.py

--- 15. Dynamic Programming/9. LIS/9. Russian doll envelopes.py
+++ 15. Dynamic Programming/9. LIS/9. Russian doll envelopes.py
@@ -19,3 +19,28 @@ class Solution(object):
                     dp[index] = max(dp[index], dp[prevIndex] + 1)
 
         return max(dp)
+
+
+
+class Solution(object):
+    def maxEnvelopes(self, envelopes):
+        if not envelopes:
+            return 0
+
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        tail = []
+        for width, height in envelopes:
+            low = 0
+            high = len(tail)-1
+            while low <= high:
+                mid = (low+high)//2
+                if tail[mid] >= height:
+                    high = mid-1
+                else:
+                    low = mid+1
+            pos = low
+            if pos == len(tail):
+                tail.append(height)
+            else:
+                tail[pos] = height
+        return len(tail)
