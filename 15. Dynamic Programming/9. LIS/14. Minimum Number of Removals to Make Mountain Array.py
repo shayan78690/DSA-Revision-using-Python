784e76f8c6300b6768325e2d699a1bb986de320d
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2026-08-12 14:48:17 +0530

    Create 14. Minimum Number of Removals to Make Mountain Array.py

---
+++ 15. Dynamic Programming/9. LIS/14. Minimum Number of Removals to Make Mountain Array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minimumMountainRemovals(self, nums):
+        n = len(nums)
+        left = [1] * n
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    left[i] = max(left[i], left[j] + 1)
+        right = [1] * n
+        for i in range(n - 1, -1, -1):
+            for j in range(n - 1, i, -1):
+                if nums[i] > nums[j]:
+                    right[i] = max(right[i], right[j] + 1)
+        max_length = 0
+        for i in range(n):
+            if left[i] > 1 and right[i] > 1:
+                max_length = max(
+                    max_length,
+                    left[i] + right[i] - 1
+                )
+
+        return n - max_length
